44cb5d85309378febeef112afa9c09d6a110e879
Clare Donnelly <namingfailed@gmail.com>
2013-04-26 16:46:28 +0100

    prep for upload

--- measurement/__init__.py
+++ measurement/__init__.py
@@ -1,13 +1,4 @@
-import inspect
-import sys
-from utilities import makeMeasureEnum
 from weight import weights
 from length import lengths
 from volume import volumes
 from item import items
-tmp=makeMeasureEnum(globals().items())
-global MeasurementEnum
-global ReverseMeasurementEnum 
-MeasurementEnum = tmp[0]
-ReverseMeasurementEnum = tmp[1]
-print ReverseMeasurementEnum
