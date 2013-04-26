40c88870b4d8e49c5391828a27292c4a10eb02a1
Clare Donnelly <namingfailed@gmail.com>
2013-04-26 15:51:58 +0100

    removed uneccessary files

--- measurement/measurements.py
+++ measurement/measurements.py
@@ -4,23 +4,40 @@
 #the measurements will need to
 #be enum for db entry
 class Measurement:
-    class LengthMeasure:
-        class Metre: pass
-        class Inch: pass
-        class Foot: pass
-    class WeightMeasure:
-        class Kilogram: pass
-        class Gram: pass
-        class ImpPound: pass
-    class VolumeMeasure:
-        class Litre: pass
-        class Millelitre: pass
-        class Cup: pass
-        class Teaspoon: pass
-        class TableSpoon: pass
-    class ItemMeasure:
-        class thickSlice: pass
-        class medSlice: pass
-        class thinSlice: pass
-
-
+    pass
+class LengthMeasure(Measurement):
+    pass
+class Metre(LengthMeasure): 
+    pass
+class Inch(LengthMeasure): 
+    pass
+class Foot(LengthMeasure): 
+    pass
+class WeightMeasure(Measurement):
+    pass
+class Kilogram(WeightMeasure):
+    pass
+class Gram(WeightMeasure): 
+    pass
+class ImpPound(WeightMeasure): 
+    pass
+class VolumeMeasure(Measurement):
+    pass
+class Litre(VolumeMeasure): 
+    pass
+class Millelitre(VolumeMeasure): 
+    pass
+class Cup(VolumeMeasure): 
+    pass
+class Teaspoon(VolumeMeasure): 
+    pass
+class TableSpoon(VolumeMeasure): 
+    pass
+class ItemMeasure(Measurement):
+    pass
+class thickSlice(ItemMeasure): 
+    pass
+class medSlice(ItemMeasure): 
+    pass
+class thinSlice(ItemMeasure): 
+    pass
